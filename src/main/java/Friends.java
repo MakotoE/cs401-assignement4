f194c1a3ef1d57e418aedc6fc7788b2856697c17
Makoto <makoto@emurasoft.com>
2021-06-09 14:30:36 -0700

    commonFriends()

--- src/main/java/Friends.java
+++ src/main/java/Friends.java
@@ -4,7 +4,7 @@ public class Friends {
 	public static Friends parseFriends(Readable file) {
 		var scanner = new Scanner(file);
 		scanner.useDelimiter("\\R");
-		var map = new HashMap<Integer, ArrayList<Integer>>();
+		var map = new HashMap<Integer, HashSet<Integer>>();
 
 		// Skip header
 		if (scanner.hasNext()) {
@@ -18,7 +18,7 @@ public class Friends {
 			var friendID = Integer.parseInt(line.substring(tabIndex + 1));
 			map.compute(userID, (id, list) -> {
 				if (list == null) {
-					return new ArrayList<>(Collections.singletonList(friendID));
+					return new HashSet<>(Collections.singletonList(friendID));
 				}
 
 				list.add(friendID);
@@ -28,23 +28,31 @@ public class Friends {
 		return new Friends(map);
 	}
 
-	private final HashMap<Integer, ArrayList<Integer>> map;
+	private final HashMap<Integer, HashSet<Integer>> map;
 
-	public Friends(HashMap<Integer, ArrayList<Integer>> map) {
+	public Friends(HashMap<Integer, HashSet<Integer>> map) {
 		this.map = map;
 	}
 
-	public Optional<Integer[]> friendsOfUser(int user) {
+	public Optional<HashSet<Integer>> friendsOfUser(int user) {
 		var value = map.get(user);
 		if (value == null) {
 			return Optional.empty();
 		}
 
-		return Optional.of(value.toArray(Integer[]::new));
+		return Optional.of(new HashSet<>(value));
 	}
 
-	public Optional<Integer[]> commonFriends(int user1, int user2) {
-		throw new UnsupportedOperationException();
+	public Optional<HashSet<Integer>> commonFriends(int user1, int user2) {
+		var user1Friends = friendsOfUser(user1);
+		var user2Friends = friendsOfUser(user2);
+
+		if (user1Friends.isEmpty() || user2Friends.isEmpty()) {
+			return Optional.empty();
+		}
+
+		user1Friends.get().retainAll(user2Friends.get());
+		return user1Friends;
 	}
 
 	public int size() {
--- src/main/java/FriendsTest.java
+++ src/main/java/FriendsTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.*;
 
 @SuppressWarnings("OptionalGetWithoutIsPresent")
 public class FriendsTest {
@@ -19,12 +20,11 @@ public class FriendsTest {
 		{
 			var result = Friends.parseFriends(new StringReader("userID\tfriendID\n2\t275"));
 			assertEquals(1, result.size());
-			assertArrayEquals(new Integer[]{275}, result.friendsOfUser(2).get());
+			assertEquals(new HashSet<>(Collections.singleton(275)), result.friendsOfUser(2).get());
 		}
 		{
-			var result = Friends.parseFriends(
-				Files.newBufferedReader(Path.of("user_friends.dat"))
-			);
+			var result =
+				Friends.parseFriends(Files.newBufferedReader(Path.of("user_friends.dat")));
 			assertEquals(1892, result.size());
 			var expected = new Integer[]{
 				275,
@@ -41,7 +41,55 @@ public class FriendsTest {
 				1625,
 				1869,
 			};
-			assertArrayEquals(expected, result.friendsOfUser(2).get());
+			assertEquals(new HashSet<>(Arrays.asList(expected)), result.friendsOfUser(2).get());
+		}
+	}
+
+	@Test
+	public void commonFriends() {
+		@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+		class Test {
+			final Friends friends;
+			final int user1;
+			final int user2;
+			final Optional<HashSet<Integer>> expected;
+
+			Test(Friends friends, int user1, int user2, Optional<HashSet<Integer>> expected) {
+				this.friends = friends;
+				this.user1 = user1;
+				this.user2 = user2;
+				this.expected = expected;
+			}
+		}
+
+		var tests = new Test[]{
+			new Test(new Friends(new HashMap<>()), 0, 0, Optional.empty()),
+			new Test(new Friends(new HashMap<>(Map.of(0, new HashSet<>(Set.of())))),
+				0,
+				0,
+				Optional.of(new HashSet<>())
+			),
+			new Test(new Friends(new HashMap<>(Map.of(0, new HashSet<>(Set.of())))),
+				0,
+				1,
+				Optional.empty()
+			),
+			new Test(new Friends(new HashMap<>(Map.of(
+				0,
+				new HashSet<>(Set.of(1)),
+				1,
+				new HashSet<>(Set.of(2))
+			))), 0, 1, Optional.of(new HashSet<>())),
+			new Test(new Friends(new HashMap<>(Map.of(
+				0,
+				new HashSet<>(Set.of(2)),
+				1,
+				new HashSet<>(Set.of(2))
+			))), 0, 1, Optional.of(new HashSet<>(Set.of(2)))),
+		};
+
+		for (var test : tests) {
+			assertEquals(test.expected, test.friends.commonFriends(test.user1, test.user2));
 		}
 	}
 }
